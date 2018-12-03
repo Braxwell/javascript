d37088c21e5faa611cc8ddd6f8b1159059242b3a
Braxwell <maxime.brachet@etu.unilim.fr>
2018-12-03 14:12:10 +0100

    Modification client.js
    Création du fichier client.html

--- client/client.js
+++ client/client.js
@@ -1,10 +1,11 @@
 const ws = new WebSocket("ws://127.0.0.1:8080/chat?pseudo=NOM", "http");
 
-ws.onopen = function (event) {
+ws.onopen = function () {
     console.log("Connecté");
+    ws.send("Un utilisateur s'est connecté");
 };
 
-ws.onclose = function (event) {
+ws.onclose = function () {
     console.log("Déconnecté");
 };
 
